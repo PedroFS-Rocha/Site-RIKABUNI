00e735e7751067e0129fa85c609135c8c210a4f8
PedroFS-Rocha <141689320+PedroFS-Rocha@users.noreply.github.com>
2025-08-30 03:08:31 -0300

    Atualizar o roteiro.js
    Adição de lógica do carrinho de compras

--- roteiro.js
+++ roteiro.js
@@ -7,5 +7,10 @@ function verProdutos() {
   }
 
   
-  
+  function adicionarAoCarrinho(nome, preco) {
+  let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+  carrinho.push({ nome, preco });
+  localStorage.setItem('carrinho', JSON.stringify(carrinho));
+  alert(`${nome} foi adicionado ao carrinho!`);
+}
   
